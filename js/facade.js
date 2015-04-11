01ae68690e7c129807442d58faf9cd51fc24faef
GoblinDeveloper <GeniusGnome@GeniusGnomedeMacBook-Air.local>
2015-04-11 19:21:57 +0800

    facade and module
    facade and module

--- js/facade.js
+++ js/facade.js
@@ -1,1 +1,31 @@
-var Facade = function(){}
+var module = (function(){
+
+  var _private_model = {
+    _i: 0,
+    set: function(i){
+      this._i = i;
+    },
+    get: function(){
+      return this._i;
+    },
+    run: function(){
+      console.log('run');
+    },
+    other: function(){
+      console.log('other');
+    }
+  };
+
+  return {
+
+    facade: function(args){
+        _private_model.set(args.val);
+        _private_model.get();
+
+        if (args.run) {
+          _private_model.run();
+        };
+    }
+  }
+
+})();
